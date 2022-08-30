664052ce192730320a398ee4f4475e2aff5d2dcb
Thquan105 <thquan105@gmail.com>
2022-08-30 22:48:41 +0700

    event button Login of ActivityLogin

--- app/src/main/java/com/example/myapplication2/ActivityLogin.java
+++ app/src/main/java/com/example/myapplication2/ActivityLogin.java
@@ -1,8 +1,11 @@
 package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 
 public class ActivityLogin extends AppCompatActivity {
@@ -11,5 +14,13 @@ public class ActivityLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final Button btlogin = (Button) findViewById(R.id.button);
+        btlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iLogin = new Intent(ActivityLogin.this,MainActivity.class);
+                startActivity(iLogin);
+            }
+        });
     }
 }
