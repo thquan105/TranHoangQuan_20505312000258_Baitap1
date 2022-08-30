60db2aed46cce599236e3b3983f27e33eb035fc0
Thquan105 <thquan105@gmail.com>
2022-08-30 22:51:41 +0700

    event icon Back of MainActivity - profile

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        final ImageView btback = (ImageView) findViewById(R.id.back_icon);
+        btback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iLogin = new Intent(MainActivity.this,ActivityLogin.class);
+                startActivity(iLogin);
+            }
+        });
     }
 
 }
